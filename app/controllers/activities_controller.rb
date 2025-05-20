c174430f483892179968dd279c66e467d930daa7
Jungha Kwon <jungha.kwon@kellogg.northwestern.edu>
2025-05-20 16:29:50 +0000

    save

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,12 +1,16 @@
 class ActivitiesController < ApplicationController
 
   def create
-    @activity = Activity.new
-    @activity["contact_id"] = params["contact_id"]
-    @activity["activity_type"] = params["activity_type"]
-    @activity["note"] = params["note"]
-    @activity.save
-    redirect_to "/contacts/#{@activity["contact_id"]}"
+    @user = User.find_by({"id" => session["user_id"]})
+    if @user != nil
+     @activity = Activity.new
+     @activity["contact_id"] = params["contact_id"]
+     @activity["activity_type"] = params["activity_type"]
+     @activity["note"] = params["note"]
+     @activity.save
+     redirect_to "/contacts/#{@activity["contact_id"]}"
+    else 
+      redirect_to "/login"
   end
 
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,15 +6,34 @@ class SessionsController < ApplicationController
   def create
     # authenticate the user
     # 1. try to find the user by their unique identifier
+     @user = user.find_by({"email" => params["email"]})
     # 2. if the user exists -> check if they know their password
+    if @user != nil
     # 3. if they know their password -> login is successful
+     if BCrypt::Password.new(@user["password"]) == params["password"]
+      # add a cookie here
+      # cookies["monster"] = "me likes cookies"
+      session["user_id"] = @user["id"]
+
+      flash["notice"] = "Welcome."
+      redirect_to "/companies"
+     else 
+      flash["notice"] = "Nope."
+      redirect_to "/login"
+     end 
+    else
     # 4. if the user doesn't exist or they don't know their password -> login fails
+     flash["notice"] = "Nope"
+     redirect_to "/login"
+     end
+    end
     flash["notice"] = "Welcome."
     redirect_to "/companies"
   end
 
   def destroy
     # logout the user
+    session["user_id"] == nil
     flash["notice"] = "Goodbye."
     redirect_to "/login"
   end
